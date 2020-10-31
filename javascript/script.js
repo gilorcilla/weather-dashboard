edaad2934581f4b7a3aaa356ce3c40d11588e803
Gil Orcilla <mathnasiumcos@gmail.com>
2020-10-30 20:21:28 -0600

    add lines of codes

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 //Global Variables
-var citiesList = $(#city-list)
+var citiesList = $("#city-list")
 var cities = []
 
 init ();
@@ -29,4 +29,65 @@ function init(){
     if (storedCities !== null){
         cities = storedCities;
     }
+    renderCities();
+
 }
+
+$(".search-button").on("click", function (event) {
+
+    event.preventDefault();
+    $("#current-day-forecast").empty();
+    $("five-day-forecast").empty();
+    let searchHistory = $("#search-term").val().trim();
+    if (searchHistory === ""){
+        return;
+    };
+
+    cities.push(searchHistory)
+    localStorage.setItem("cities", JSON.stringify(cities));
+    queryURL = buildQueryUrl(searchHistory);
+    let fiveDayQueryURL;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    .then(function (fiveData){
+        buildFiveDayForecast(fiveData)
+    })
+
+    var uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi?" + "lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=804a431c119b5a58e4b92afb5b02a0c7"
+
+    buildCurrentWeatherCard(searchHistory);
+
+    $.ajax({
+        url: uvQueryURL,
+        method: "GET"
+    })
+    .then(function (response){
+        let uvIndexEl = response.value
+        uvIndexTag = $("<p>").text("UV Index: " + uvIndexEl)
+        $(".current-day-weather").append(uvIndexTag)
+    })
+    $("#search-term").val(null)
+    init();
+
+    });
+
+$("#city-list").on("click", "button", function(){
+
+    $("#current-day-forecast").empty();
+    $("#five-day-forecast").empty();
+    let searchHistory = $(this).text();
+    queryURLHist  = buildQueryUrl(searchHistory);
+
+    $.ajax({
+        url: queryURLHist,
+        method: "GET"
+    })
+    .then(function (data){
+        buildCurrentWeatherCard(data)
+        let uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi?" + "lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=804a431c119b5a58e4b92afb5b02a0c7"
+    
+    $.ajax({
+        
+    })
