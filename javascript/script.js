a94f3456b8447a0704e7d224449dcc408064d301
Gil Orcilla <mathnasiumcos@gmail.com>
2020-11-02 18:39:07 -0700

    revised the script and html

--- javascript/script.js
+++ javascript/script.js
@@ -3,6 +3,8 @@ var citiesList = $("#city-list")
 var cities = []
 
 init ();
+
+//Local Storage
 function  renderCities(){
     if (cities.length > 5){
         cities.shift();
@@ -25,6 +27,7 @@ function  renderCities(){
 function init(){
 
     $("city-list").empty();
+
     let storedCities = JSON.parse(localStorage.getItem("cities"));
     if (storedCities !== null){
         cities = storedCities;
@@ -33,32 +36,93 @@ function init(){
 
 }
 
+
 $(".search-button").on("click", function (event) {
-
     event.preventDefault();
-    $("#current-day-forecast").empty();
-    $("five-day-forecast").empty();
-    let searchHistory = $("#search-term").val().trim();
-    if (searchHistory === ""){
-        return;
-    };
-
-    cities.push(searchHistory)
+ var city= $("#search-term").val();
+    currentWeather(city)
+    forecastWeather(city)
+    // $("#current-day-forecast").empty();
+    // $("five-day-forecast").empty();
+    // let searchHistory = $("#search-term").val().trim();
+  
+    let cities = JSON.parse(localStorage.getItem("cities"));
+    cities.push(city)
     localStorage.setItem("cities", JSON.stringify(cities));
-    queryURL = buildQueryUrl(searchHistory);
-    let fiveDayQueryURL;
+    $("#search-term").val(null)
+})
+
+    function currentWeather(city){
+    queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city +"&appid=d505d181bc232a369cacbc75835c8e23&units=imperial"
+    
+    // Project1(Coding Bootcamp)
+    // d505d181bc232a369cacbc75835c8e23"
+    // let fiveDayQueryURL;
     $.ajax({
         url: queryURL,
         method: "GET"
     })
     .then(function (fiveData){
-        buildFiveDayForecast(fiveData)
-        //console.log(fiveData);
+        // buildFiveDayForecast(fiveData)
+        console.log(fiveData);
+        
+        $("#current-day-forecast").html(`
+        <div>
+        <h1>${city}</h1>
+        <h3>Temp: ${fiveData.main.temp}</h3>
+        <img src="" />
+        <h1>windspeed${fiveData.wind.speed}</h1>
+        <h1>humidity${fiveData.main.humidity}</h1>
+        <h1>description${fiveData.weather[0].description}</h1>
+        </div>
+        
+        
+        `)
+        var lat = fiveData.coord.lat
+        var long = fiveData.coord.lon
+        uvInfo(lat,long)
     })
+
+}
+function forecastWeather(city){
+    queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+city +"&appid=d505d181bc232a369cacbc75835c8e23&units=imperial"
+    // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+    // Project1(Coding Bootcamp)
+    // d505d181bc232a369cacbc75835c8e23"
+    // let fiveDayQueryURL;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    .then(function (fiveData){
+        // buildFiveDayForecast(fiveData)
+        console.log(fiveData);
+        var data=fiveData.list
+        var htmlString=""
+        for(let i=0; i<data.length; i=i+8){
+            htmlString+=`<div>
+            <h1>${data[i].dt_txt}</h1>
+            <h3>Temp: ${data[i].main.temp}</h3>
+            <img src="" />
+            <h1>windspeed${data[i].wind.speed}</h1>
+            <h1>humidity${data[i].main.humidity}</h1>
+            <h1>description${data[i].weather[0].description}</h1>
+            </div>`
+
+        }
+        
+        
+        
+        
+        console.log(htmlString)
+        $("#five-day-forecast").html(htmlString)
+    })
+}
+function uvInfo(){
 
     var uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi?" + "lat=" + data.coord.lat + "&long=" + data.coord.long + "&appid=d505d181bc232a369cacbc75835c8e23"
 
-    buildCurrentWeatherCard(searchHistory);
+   
 
     $.ajax({
         url: uvQueryURL,
@@ -67,13 +131,13 @@ $(".search-button").on("click", function (event) {
     .then(function (response){
         let uvIndexEl = response.value
         uvIndexTag = $("<p>").text("UV Index: " + uvIndexEl)
-        $(".current-day-weather").append(uvIndexTag)
+        $("#uv").html(uvIndexTag)
         //console.log(response);
     })
-    $("#search-term").val(null)
-    init();
+ 
+    
 
-    });
+}
 
 $("#city-list").on("click", "button", function(){
 
@@ -113,16 +177,4 @@ $("#city-list").on("click", "button", function(){
     })
 })
 
-
-/*$.ajax({
-            url: "https://api.openweathermap.org/data/2.5/onecall",
-            data: {
-                q: searchCity,
-                appid: "d505d181bc232a369cacbc75835c8e23",
-                units: "imperial",
-            },
-            success: function( result ) {
-                console.log(result)
-            }
-        });*/
 
